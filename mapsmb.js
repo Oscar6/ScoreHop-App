4e0ebf180675d07aa55f43fb459465394ab9dd85
Oscar6 <oscar.m1024@gmail.com>
2018-12-28 20:35:59 -0600

    updated search and markers

--- mapsmb.js
+++ mapsmb.js
@@ -1,130 +1,128 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoib3NjYXI2IiwiYSI6ImNqcHc0Nm1iejAxYmozeGxzcDM3MmQ1ZmYifQ.SpDPryg7o1dzi6sgVlO0GQ'; // replace this with your access token
+// Set starting coordinates
+var map1 = L.map('map').setView([29.7604, -95.3698], 12);
 
-// Coordinates longitude - latitude
-var geojson = {
-    type: 'FeatureCollection',
-    features: [{
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [-95.3560, 29.7487]
-        },
-        properties: {
-            title: '8th Wonder Brewery',
-            // description: 'Near you',
-            "iconSize": [25, 25]
-        }
+// Dont touch
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoib3NjYXI2IiwiYSI6ImNqcHc0Nm1iejAxYmozeGxzcDM3MmQ1ZmYifQ.SpDPryg7o1dzi6sgVlO0GQ',
+        
+}).addTo(map1);
+
+// Blue marker. Iniate on search function.
+function venueMarker(lat,log,name){
+L.marker([lat,log], 
+    {bounceOnAdd: true}).
+    bindPopup(`<b>${name}</b>`).
+    openPopup().addTo(map1);
+}
+// Beer Icon styling
+var beerIcon = L.icon({
+    iconUrl: 'icons/colormug.jpg',
+    iconSize: [30, 30],
+    // color: rgb(255,69,0),
+    iconAnchor: [15, 15],
+    popupAnchor: [0, -10],
+    // shadowSize: [68, 95],
+    // shadowAnchor: [22, 94]
+});
+
+// Hard coded brewery examples
+L.marker([29.7487, -95.3560], {icon: beerIcon, bounceOnAdd: true}).bindPopup("<b>8th Wonder Brewery</b>").openPopup().addTo(map1);
+L.marker([29.7492, -95.3435], {icon: beerIcon, bounceOnAdd: true}).bindPopup("<b>Sigma Brewing Company</b>").openPopup().addTo(map1);
+L.marker([29.8058, -95.4608], {icon: beerIcon, bounceOnAdd: true}).bindPopup("<b>Karbach Brewing Co.</b>").openPopup().addTo(map1);
+L.marker([29.7711, -95.3486], {icon: beerIcon, bounceOnAdd: true}).bindPopup("<b>Saint Arnold Brewing Company</b>").openPopup().addTo(map1);
+
+
+// Logo Watermark
+// logo position: bottomright, topright, topleft, bottomleft
+L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create('img');
+
+        img.src = 'icons/logofinal.png';
+        img.style.width = '200px';
+
+        return img;
     },
-    {
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [-95.3435, 29.7492]
-        },
-        properties: {
-            title: 'Sigma Brewing Company',
-            // description: 'Near you',
-            "iconSize": [25, 25]
-        }
-    },
-    {
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [-95.4608, 29.8058]
-        },
-        properties: {
-            title: 'Karbach Brewing Co.',
-            // description: 'Near you',
-            "iconSize": [25, 25]
-        }
-    },
-    {
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [-95.3486, 29.7711]
-        },
-        properties: {
-            title: 'Saint Arnold Brewing Company',
-            // description: 'Near you',
-            "iconSize": [25, 25]
-        }
-    }]
-};
 
-// Start Location
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-95.3698, 29.7604],
-    zoom: 11
+    onRemove: function(map) {
+        // Nothing to do here
+    }
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+L.control.watermark = function(opts) {
+    return new L.Control.Watermark(opts);
+}
 
-// Search bar
-var geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-});
-
-document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-
-// Places a point to location searched
-map.on('load', function() {
-    map.addSource('single-point', {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": []
-        }
-    });
-
-    map.addLayer({
-        "id": "point",
-        "source": "single-point",
-        "type": "circle",
-        "paint": {
-            "circle-radius": 10,
-            "circle-color": "#2FF37A"
-        }
-    });
-
-    // Listen for the `result` event from the MapboxGeocoder that is triggered when a user
-    // makes a selection and add a symbol that matches the result.
-    geocoder.on('result', function(ev) {
-        map.getSource('single-point').setData(ev.result.geometry);
-    });
-});
+L.control.watermark({ position: 'bottomright' }).addTo(map1);
 
 
-var popup = new mapboxgl.Popup({ offset: 38 })
-    .setText('Houston, Texas');
-    
+//Search bar js 
+$(function () {
 
-var marker = new mapboxgl.Marker({color:'red'})
-    .setLngLat([-95.3698, 29.7604])
-    .setPopup(popup)
-    .addTo(map);
+    $('#button-addon2').click(function () {
+        $('li').remove()
+        var coordsarr = [];
 
-geojson.features.forEach(function(marker) {
-    // create a DOM element for the marker
-    var popup = new mapboxgl.Popup({offset: 10})
-    .setHTML('<b>' + marker.properties.title + '</b>');
+        var interest = $('#event').val()
+        var usercity = $("#city").val()
+        var $unorder = $("#unorder-list")
+        //   $.get(`https://api.seatgeek.com/2/events?client_id=MTQ0OTYyNTZ8MTU0NTI2OTM2NS4yNg`)
+        $.get(`https://api.seatgeek.com/2/events?q=${interest}&client_id=MTQ0OTYyNTZ8MTU0NTI2OTM2NS4yNg`)
+            .done(result => {
+                console.log(result)
+                $.each(result.events, function (index, value) {
+                    //    if(value.venue.city==usercity){    
+                    console.log(value.title)
+                    console.log(value.venue.name)
+                    console.log(new Date(value.datetime_utc))
+                    //  console.log(new Date(value.datetime_local))
+                    date = new Date(value.datetime_local)
+                    console.log(value.venue.address + " " + value.venue.extended_address)
+                    console.log(value.venue.id)
+                    console.log(`lat=` + value.venue.location.lat)
+                    console.log(`lon=` + value.venue.location.lon)
+                    $unorder.append("<li><div class='card bg-info text-white'>" +
+                        "<div class='card-body'>" + "<a style='color:white' id=" + value.venue.id + " href='#'>" +
+                        "<p>" + value.title + "</p>" +
+                        "<p>" + new Date(value.datetime_utc) + "</p>" +
+                        "<p>" + value.venue.address + " " + value.venue.extended_address + "</p>" +
+                        "</a></div></div><br></li>")// end of append
+                    var coords = {}
+                    coords['lat'] = value.venue.location.lat
+                    coords['lon'] = value.venue.location.lon
+                    coords['id'] = value.venue.id
+                    coords['venue'] = value.venue.name
+                    coordsarr.push(coords)
+                    console.log(coords)
+                    console.log(coordsarr)
+                    //    }
+                })// end of each
 
-    var beer = document.createElement('div');
-    beer.className = 'beerIcon';
-    beer.style.backgroundImage = 'url(/icons/beermug.svg)' + marker.properties.iconSize;
-    beer.style.width = marker.properties.iconSize[0] + 'px';
-    beer.style.height = marker.properties.iconSize[1] + 'px';
+                $("a").click(function (event) {
+                    event.preventDefault()
 
-    // beer.addEventListener('click', function() {
-    //     window.alert(marker.properties.message);
-    // });
+                    console.log(coordsarr)
+                    console.log(event)
+                    //var idval=this.id
+                    var idval = $(this).attr('id');
+                    //
+                    coordsarr.map(function (arr, index) {
+                        if (arr['id'] == idval) {
+                            venueMarker(arr['lat'], arr['lon'], arr['venue'])
+                            $('li').remove()
+                            console.log(arr['lat'])
+                            console.log(arr['lon'])
+                            console.log(arr['venue'])
+                        }
+                    })
 
-    // add marker to map
-    new mapboxgl.Marker(beer)
-        .setLngLat(marker.geometry.coordinates)
-        .setPopup(popup)
-        .addTo(map);
-});
+
+                })
+            })//end of done
+
+
+    })//end of click    
+})//end of Jquery
