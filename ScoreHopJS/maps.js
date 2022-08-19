e656524c90ef219330d7e40b5fa33566ef98a68a
Oscar6 <oscar.m1024@gmail.com>
2022-08-19 15:29:40 -0500

    updating deprecated tiles and parameters

--- ScoreHopJS/maps.js
+++ ScoreHopJS/maps.js
@@ -2,13 +2,12 @@
 var map1 = L.map("map").setView([29.7604, -95.3698], 12);
 
 // Dont touch
-L.tileLayer(
-  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox/streets-v11",
     accessToken:
       "pk.eyJ1Ijoib3NjYXI2IiwiYSI6ImNqcHc0Nm1iejAxYmozeGxzcDM3MmQ1ZmYifQ.SpDPryg7o1dzi6sgVlO0GQ"
   }
